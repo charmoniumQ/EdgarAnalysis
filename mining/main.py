31ed5e73d267458282206b08ac8fa7cdc2d03930
charmoniumQ <sag150430@utdallas.edu>
2016-10-19 01:24:35 -0500

    wrote script to download index file

---
+++ mining/main.py
@@ -0,0 +1,13 @@
+from itertools import islice
+from retrieve import get_index
+
+# get the master index for the third quarter of 2016
+# the first time you run this, it will take a long time to download the data
+# every subsequent time, it will use the cached data
+index = get_index(2016, 3, 'master')
+for i, row in zip(range(0, 10), index):
+    print(row)
+
+index = get_index(2016, 3, 'company')
+for i, row in zip(range(0, 10), index):
+    print(row)
